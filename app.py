04aa196e46d35636dd1a0969971dc7014f975379
Daniel Mikusa <dmikusa@gopivotal.com>
2014-07-14 13:35:00 -0400

    Added first cut at stat tracking.

--- app.py
+++ app.py
@@ -9,10 +9,12 @@
 import os
 import os.path as osp
 import requests
+from collections import defaultdict
 from flask import Flask
 from flask import Response
 from flask import abort
 from flask import render_template
+
 
 app = Flask(__name__)
 
@@ -20,24 +22,61 @@ app.config['DEBUG'] = True
 app.config['PROXY_ROOT'] = os.environ['PROXY_ROOT']
 app.config['DL_BUFFER_SIZE'] = 2 * 1024  # in bytes
 
+
+def stat():
+    return {
+        'TOTAL': 0,
+        'CACHED': 0,
+        'PROXIED': 0
+    }
+
+
+# Stats Holder
+stat_data = {
+    'TOTAL': 0,
+    'CACHED': 0,
+    'PROXIED': 0,
+    'BY_FILE': defaultdict(stat),  # same stats, but per file
+    'BY_CODE': defaultdict(stat)   # same stats, but per code
+}
+
+
+def update_stats(path, cached=False, code=200):
+    stat_data['TOTAL'] += 1
+    stat_data['BY_FILE'][path]['TOTAL'] += 1
+    stat_data['BY_CODE'][code]['TOTAL'] += 1
+    if cached:
+        stat_data['CACHED'] += 1
+        stat_data['BY_FILE'][path]['CACHED'] += 1
+        stat_data['BY_CODE'][code]['CACHED'] += 1
+    else:
+        stat_data['PROXIED'] += 1
+        stat_data['BY_FILE'][path]['PROXIED'] += 1
+        stat_data['BY_CODE'][path]['PROXIED'] += 1
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/stats")
 def stats():
-    return render_template('stats.html')
+    return render_template('stats.html', stats=stat_data)
+
 
 @app.route("/browse", defaults={'path': None})
 @app.route("/browse/<path:path>")
 def browse(path):
     return render_template('browse.html')
 
+
 @app.route("/files/<path:path>")
 def files(path):
     full_path = osp.abspath(osp.join(app.static_folder, 'files', path))
     if osp.exists(full_path):
         if osp.isfile(full_path):
+            update_stats(path, cached=True)
             return app.send_static_file(osp.join('files', path))
         elif osp.isdir(full_path):
             return "Directory Index"  # redirect to browse
@@ -50,6 +89,7 @@ def files(path):
         r = requests.get(osp.join(app.config['PROXY_ROOT'], path), stream=True)
         # if request is OK, stream it and save to disk, otherwise fail
         if r.status_code == 200:
+            update_stats(path, cached=False)
             def generate():
                 with open(full_path, 'wb') as fd:
                     for chunk in r.iter_content(app.config['DL_BUFFER_SIZE']):
@@ -57,7 +97,9 @@ def files(path):
                         yield chunk
             return Response(generate(), mimetype='application/octet-stream')
         else:
+            update_stats(path, cached=False, code=r.status_code)
             abort(r.status_code)
+
 
 if __name__ == "__main__":
     app.run()
